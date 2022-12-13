ce3150747530990425977b9822bf608c667c7ff9
Carlo-Antonio Marin <cajmrn@gmail.com>
2022-12-13 14:53:31 -0400

    committing so I can revert :D

--- hfg2/src/main.ts
+++ hfg2/src/main.ts
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import DaySpanVuetify from 'dayspan-vuetify'
 import { loadFonts } from './plugins/webfontloader'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -13,4 +14,9 @@ createApp(App)
   .use(router)
   .use(vuetify)
   .use(createPinia())
+  .use(DaySpanVuetify,{
+    methods:{
+      getDefaultEventColor: () => '#1976d2'
+    }
+  })
   .mount('#app')
