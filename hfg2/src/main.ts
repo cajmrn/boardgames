e77ab5569b976e8b35c42f6936431b9a65c50d49
Carlo-Antonio Marin <cajmrn@gmail.com>
2022-12-13 15:22:41 -0400

    reverting

--- hfg2/src/main.ts
+++ hfg2/src/main.ts
@@ -3,9 +3,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import vuetify from './plugins/vuetify'
-import DaySpanVuetify from 'dayspan-vuetify'
-import { loadFonts } from './plugins/webfontloader'
+
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/main.css'
